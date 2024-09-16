f0b9399b6ea7dd30ec50a96d284a3ccaad271cc5
Fabio Amadio <fabioamadio93@gmail.com>
2024-09-16 14:47:34 +0200

    Config docker

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -10,5 +10,4 @@ docker run \
             --privileged \
             --volume /tmp/.X11-unix:/tmp/.X11-unix \
             --name opensot_fsm \
-            --volume ${HOME}/Workspace/inria_fsm_cartesio:/home/forest_ws/src/inria_fsm_cartesio \
             opensot_fsm
