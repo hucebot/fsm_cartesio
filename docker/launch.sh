67297cc2e1ff8da6882c84c40a5f9ae869dada5e
Fabio Amadio <fabioamadio93@gmail.com>
2024-09-20 17:19:41 +0200

    Refactor docker scripts

---
+++ docker/launch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+IsRunning=`docker ps -f name=cartesio_tiago_dual | grep -c "cartesio"`;
+if [ $IsRunning -eq "0" ]; then
+    xhost +local:docker
+    docker run --rm \
+        --gpus all \
+        -e DISPLAY=$DISPLAY \
+        -e XAUTHORITY=$XAUTHORITY \
+        -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+        -e NVIDIA_DRIVER_CAPABILITIES=all \
+        -e 'QT_X11_NO_MITSHM=1' \
+        -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+        -v /tmp/docker_share:/tmp/docker_share \
+        -v `pwd`/..:/home/catkin_ws/src/fsm_cartesio \
+        --ipc host \
+        --device /dev/dri \
+        --device /dev/snd \
+        --device /dev/input \
+        --device /dev/bus/usb \
+        --privileged \
+        --ulimit rtprio=99 \
+        --net host \
+        --name cartesio_tiago_dual \
+        --entrypoint /bin/bash \
+        -ti cartesio:tiago_dual
+else
+    echo "Docker image is already running. Opening new terminal...";
+    docker exec -ti cartesio_tiago_dual /bin/bash
+fi
