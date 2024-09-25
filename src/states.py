04df50b89d876a20ebf5c7cd7b4f3f01a5b2c0ac
Fabio Amadio <fabioamadio93@gmail.com>
2024-09-25 18:16:04 +0200

    Changed FollowWaypoints states implementation

--- src/states.py
+++ src/states.py
@@ -162,20 +162,57 @@ class FollowWaypoints(smach.State):
             task = userdata.client.getTask(userdata.task_name)
             task.setBaseLink(userdata.task_base_link)
 
-            timeout = 0.0
-            for wp in userdata.waypoints:
-                timeout += wp.time
-
-            task.setWayPoints(userdata.waypoints)
-            task.waitReachCompleted(
-                timeout
-            )  # blocks till action is completed (or timeout has passed)
+            for i in range(len(userdata.waypoints)):
+                point = Affine3()
+                point.translation = userdata.waypoints[i].frame.translation
+                point.quaternion = userdata.waypoints[i].frame.quaternion
+                time = userdata.waypoints[i].time
+                task.setPoseTarget(point, time)
+                task.waitReachCompleted(
+                    time
+                )  # blocks till action is completed (or timeout has passed)
 
             return "success"
         except Exception as error:
             smach.logerr("An error occurred: " + type(error).__name__)
             smach.logerr(error)
             return "fail"
+
+
+# TODO: Implementation using CartesI/O's 'setWayPoints' method, check for bugs!
+# class FollowWaypoints(smach.State):
+#     def __init__(self):
+#         smach.State.__init__(
+#             self,
+#             outcomes=["success", "fail"],
+#             input_keys=[
+#                 "client",  # CartesI/O client (type: 'cartesian_interface.pyci.CartesianInterfaceRos')
+#                 "task_name",  # Task name (type: 'str')
+#                 "task_base_link",  # Base link for the task (type: 'str')
+#                 "waypoints",  # List of waypoints (type: 'list' of 'cartesian_interface.pyci.WayPoint')
+#             ],
+#         )
+
+#     def execute(self, userdata):
+#         try:
+#             userdata.client.update()
+#             task = userdata.client.getTask(userdata.task_name)
+#             task.setBaseLink(userdata.task_base_link)
+
+#             timeout = 0.0
+#             for wp in userdata.waypoints:
+#                 timeout += wp.time
+
+#             task.setWayPoints(userdata.waypoints)
+#             task.waitReachCompleted(
+#                 timeout
+#             )  # blocks till action is completed (or timeout has passed)
+
+#             return "success"
+#         except Exception as error:
+#             smach.logerr("An error occurred: " + type(error).__name__)
+#             smach.logerr(error)
+#             return "fail"
 
 
 class FollowTrajectory(smach.State):
@@ -295,82 +332,166 @@ class FollowWaypointsFromCfg(smach.State):
         )
 
     def execute(self, userdata):
-        try:
-            userdata.client.update()
-            with open(userdata.config_path, "r") as file:
-                config = yaml.safe_load(file)
-            motion_def = config[userdata.tag]
-            ref_frame = motion_def["ref_frame"]
-            task_name = motion_def["task"]
-            task_base_link = motion_def["task_base_link"]
-            task = userdata.client.getTask(task_name)
-            task.setBaseLink(task_base_link)
+        # try:
+        userdata.client.update()
+        with open(userdata.config_path, "r") as file:
+            config = yaml.safe_load(file)
+        motion_def = config[userdata.tag]
+        ref_frame = motion_def["ref_frame"]
+        task_name = motion_def["task"]
+        task_base_link = motion_def["task_base_link"]
+        task = userdata.client.getTask(task_name)
+        task.setBaseLink(task_base_link)
 
-            offs_translation = np.array(motion_def["offset"]["translation"]).reshape(
-                -1, 3
-            )
-            offs_rotation = np.array(motion_def["offset"]["rotation"]).reshape(-1, 4)
-            time = np.array(motion_def["offset"]["time"]).reshape(-1, 1)
+        offs_translation = np.array(motion_def["offset"]["translation"]).reshape(-1, 3)
+        offs_rotation = np.array(motion_def["offset"]["rotation"]).reshape(-1, 4)
+        time = np.array(motion_def["offset"]["time"]).reshape(-1, 1)
 
-            if not (
-                offs_translation.shape[0] == offs_rotation.shape[0] == time.shape[0]
-            ):
-                smach.logwarn("Inconsistent number of elements while parsing target")
-                return "fail"
-
-            waypoints = []
-            timeout = 0.0
-            if ref_frame == task_base_link:
-                # No offset
-                translation = offs_translation
-                rotation = offs_rotation
-                # Parsing waypoints
-                for i in range(translation.shape[0]):
-                    wp = pyci.WayPoint()
-                    wp.frame.translation = translation[i, :]
-                    wp.frame.quaternion = rotation[i, :]
-                    wp.time = time[i, :]
-                    timeout += wp.time
-                    waypoints.append(wp)
-            else:
-                # Express waypoints in base_link (from ref_frame)
-                t = userdata.tf_buffer.lookup_transform(
-                    userdata.tf_prefix + task_base_link,
-                    userdata.tf_prefix + ref_frame,
-                    rospy.Time(),
-                )
-                x = t.transform.translation.x
-                y = t.transform.translation.y
-                z = t.transform.translation.z
-                qx = t.transform.rotation.x
-                qy = t.transform.rotation.y
-                qz = t.transform.rotation.z
-                qw = t.transform.rotation.w
-                base_link_to_ref = Affine3()
-                base_link_to_ref.translation = np.array([x, y, z])
-                base_link_to_ref.quaternion = np.array([qx, qy, qz, qw])
-                # Parsing waypoints
-                for i in range(offs_translation.shape[0]):
-                    offset = Affine3()
-                    offset.translation = offs_translation[i, :]
-                    offset.quaternion = offs_rotation[i, :]
-                    point = base_link_to_ref * offset
-                    wp = pyci.WayPoint()
-                    wp.frame.translation = point.translation
-                    wp.frame.quaternion = point.quaternion
-                    wp.time = time[i, :]
-                    timeout += wp.time
-                    waypoints.append(wp)
-
-            task.setWayPoints(waypoints)
-            task.waitReachCompleted(
-                timeout
-            )  # blocks till action is completed (or timeout has passed)
-            return "success"
-        except Exception as error:
-            smach.logerr("An error occurred: " + type(error).__name__)
-            smach.logerr(error)
+        if not (offs_translation.shape[0] == offs_rotation.shape[0] == time.shape[0]):
+            smach.logwarn("Inconsistent number of elements while parsing target")
             return "fail"
+
+        if ref_frame == task_base_link:
+            # No offset
+            translation = offs_translation
+            rotation = offs_rotation
+            # Parsing waypoints
+            for i in range(translation.shape[0]):
+                point = Affine3()
+                point.translation = translation[i, :]
+                point.quaternion = rotation[i, :]
+                task.setPoseTarget(point, time[i, :])
+                task.waitReachCompleted(
+                    time[i, :]
+                )  # blocks till action is completed (or timeout has passed)
+        else:
+            # Express waypoints in base_link (from ref_frame)
+            t = userdata.tf_buffer.lookup_transform(
+                userdata.tf_prefix + task_base_link,
+                userdata.tf_prefix + ref_frame,
+                rospy.Time(),
+            )
+            x = t.transform.translation.x
+            y = t.transform.translation.y
+            z = t.transform.translation.z
+            qx = t.transform.rotation.x
+            qy = t.transform.rotation.y
+            qz = t.transform.rotation.z
+            qw = t.transform.rotation.w
+            base_link_to_ref = Affine3()
+            base_link_to_ref.translation = np.array([x, y, z])
+            base_link_to_ref.quaternion = np.array([qx, qy, qz, qw])
+            # Parsing waypoints
+            for i in range(offs_translation.shape[0]):
+                offset = Affine3()
+                offset.translation = offs_translation[i, :]
+                offset.quaternion = offs_rotation[i, :]
+                point = base_link_to_ref * offset
+                task.setPoseTarget(point, time[i, :])
+                task.waitReachCompleted(
+                    time[i, :]
+                )  # blocks till action is completed (or timeout has passed)
+
+        return "success"
+        # except Exception as error:
+        #     smach.logerr("An error occurred: " + type(error).__name__)
+        #     smach.logerr(error)
+        #     return "fail"
+
+
+# TODO: Implementation using CartesI/O's 'setWayPoints' method, check for bugs!
+# class FollowWaypointsFromCfg(smach.State):
+#     def __init__(self):
+#         smach.State.__init__(
+#             self,
+#             outcomes=["success", "fail"],
+#             input_keys=[
+#                 "client",  # CartesI/O client (type: 'cartesian_interface.pyci.CartesianInterfaceRos')
+#                 "tf_prefix",  # CartesI/O tf_prefix (type: 'str')
+#                 "tf_buffer",  # tf2 buffer (type: 'tf2_ros.buffer.Buffer')
+#                 "config_path",  # Path to the yaml file with targets definition (type: 'str')
+#                 "tag",  # Tag of the desired motion in the config file (type: 'str')
+#             ],
+#             output_keys=["tf_buffer"],
+#         )
+
+#     def execute(self, userdata):
+#         try:
+#             userdata.client.update()
+#             with open(userdata.config_path, "r") as file:
+#                 config = yaml.safe_load(file)
+#             motion_def = config[userdata.tag]
+#             ref_frame = motion_def["ref_frame"]
+#             task_name = motion_def["task"]
+#             task_base_link = motion_def["task_base_link"]
+#             task = userdata.client.getTask(task_name)
+#             task.setBaseLink(task_base_link)
+
+#             offs_translation = np.array(motion_def["offset"]["translation"]).reshape(
+#                 -1, 3
+#             )
+#             offs_rotation = np.array(motion_def["offset"]["rotation"]).reshape(-1, 4)
+#             time = np.array(motion_def["offset"]["time"]).reshape(-1, 1)
+
+#             if not (
+#                 offs_translation.shape[0] == offs_rotation.shape[0] == time.shape[0]
+#             ):
+#                 smach.logwarn("Inconsistent number of elements while parsing target")
+#                 return "fail"
+
+#             waypoints = []
+#             timeout = 0.0
+#             if ref_frame == task_base_link:
+#                 # No offset
+#                 translation = offs_translation
+#                 rotation = offs_rotation
+#                 # Parsing waypoints
+#                 for i in range(translation.shape[0]):
+#                     wp = pyci.WayPoint()
+#                     wp.frame.translation = translation[i, :]
+#                     wp.frame.quaternion = rotation[i, :]
+#                     wp.time = time[i, :]
+#                     timeout += wp.time
+#                     waypoints.append(wp)
+#             else:
+#                 # Express waypoints in base_link (from ref_frame)
+#                 t = userdata.tf_buffer.lookup_transform(
+#                     userdata.tf_prefix + task_base_link,
+#                     userdata.tf_prefix + ref_frame,
+#                     rospy.Time(),
+#                 )
+#                 x = t.transform.translation.x
+#                 y = t.transform.translation.y
+#                 z = t.transform.translation.z
+#                 qx = t.transform.rotation.x
+#                 qy = t.transform.rotation.y
+#                 qz = t.transform.rotation.z
+#                 qw = t.transform.rotation.w
+#                 base_link_to_ref = Affine3()
+#                 base_link_to_ref.translation = np.array([x, y, z])
+#                 base_link_to_ref.quaternion = np.array([qx, qy, qz, qw])
+#                 # Parsing waypoints
+#                 for i in range(offs_translation.shape[0]):
+#                     offset = Affine3()
+#                     offset.translation = offs_translation[i, :]
+#                     offset.quaternion = offs_rotation[i, :]
+#                     point = base_link_to_ref * offset
+#                     wp = pyci.WayPoint()
+#                     wp.frame.translation = point.translation
+#                     wp.frame.quaternion = point.quaternion
+#                     wp.time = time[i, :]
+#                     timeout += wp.time
+#                     waypoints.append(wp)
+
+#             task.setWayPoints(waypoints)
+#             task.waitReachCompleted(
+#                 timeout
+#             )  # blocks till action is completed (or timeout has passed)
+#             return "success"
+#         except Exception as error:
+#             smach.logerr("An error occurred: " + type(error).__name__)
+#             smach.logerr(error)
+#             return "fail"
 
 
 class FollowTrajectoryFromCfg(smach.State):
@@ -403,7 +524,9 @@ class PalGripperGrasp(smach.State):
 
     def execute(self, userdata):
         try:
-            srv_proxy = rospy.ServiceProxy(userdata.parallel_gripper_controller + "/grasp", Empty)
+            srv_proxy = rospy.ServiceProxy(
+                userdata.parallel_gripper_controller + "/grasp", Empty
+            )
             req = EmptyRequest()
             srv_proxy(req)
             return "success"
